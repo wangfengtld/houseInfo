8f8ad010c3085add941868952796dfbc408ec9d5
wangfeng <wangfengtld@126.com>
2018-06-05 11:37:14 +0800

    增加flash动画

---
+++ app/src/main/java/com/tl/houseinfo/FlashActivity.java
@@ -0,0 +1,22 @@
+package com.tl.houseinfo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class FlashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(FlashActivity.this,  MainActivity.class));
+            }
+        }, 1000);
+    }
+}
